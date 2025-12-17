fdc9ed5ab51aa6fcd4c8b0d8ca65c53c39dd3856
Gagan G <gnawalign@gmail.com>
2025-12-17 15:39:46 +0000

    feat: LondonBusEntity extends AbstractHorseEntity for predefined movement physics

--- src/main/java/net/gagan/londonbus/LondonBusEntity.java
+++ src/main/java/net/gagan/londonbus/LondonBusEntity.java
@@ -1,30 +1,11 @@
 package net.gagan.londonbus;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.vehicle.VehicleEntity;
-import net.minecraft.item.Item;
-import net.minecraft.storage.ReadView;
-import net.minecraft.storage.WriteView;
+import net.minecraft.entity.passive.AbstractHorseEntity;
 import net.minecraft.world.World;
 
-public class LondonBusEntity extends VehicleEntity {
-
-  public LondonBusEntity(EntityType<?> entityType, World world) {
+public class LondonBusEntity extends AbstractHorseEntity {
+  protected LondonBusEntity(EntityType<? extends AbstractHorseEntity> entityType, World world) {
     super(entityType, world);
-  }
-
-  @Override
-  protected Item asItem() {
-    return null;
-  }
-
-  @Override
-  protected void readCustomData(ReadView view) {
-
-  }
-
-  @Override
-  protected void writeCustomData(WriteView view) {
-
   }
 }

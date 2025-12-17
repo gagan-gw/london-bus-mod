1738b493db72257848881bd5f10d86ccb8c5f052
Gagan G <gnawalign@gmail.com>
2025-12-17 17:10:21 +0000

    feat: allows any player to control the bus

--- src/main/java/net/gagan/londonbus/LondonBusEntity.java
+++ src/main/java/net/gagan/londonbus/LondonBusEntity.java
@@ -1,11 +1,33 @@
 package net.gagan.londonbus;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.AbstractHorseEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
+import org.jspecify.annotations.Nullable;
 
 public class LondonBusEntity extends AbstractHorseEntity {
   protected LondonBusEntity(EntityType<? extends AbstractHorseEntity> entityType, World world) {
     super(entityType, world);
+  }
+
+  @Override
+  public ActionResult interactMob(PlayerEntity player, Hand hand) {
+    bondWithPlayer(player);
+    return super.interactMob(player, hand);
+  }
+
+  @Override
+  public @Nullable LivingEntity getControllingPassenger() {
+    Entity var2 = this.getFirstPassenger();
+    if (var2 instanceof PlayerEntity) {
+      PlayerEntity playerEntity = (PlayerEntity)var2;
+      return playerEntity;
+    }
+    return super.getControllingPassenger();
   }
 }
